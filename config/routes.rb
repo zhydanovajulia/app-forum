310de80483e5afe33279bde3657b5a08472d0898
julia <zhydanovajulia@gmail.com>
2011-10-29 17:31:20 +0300

    Done with sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 AppForum::Application.routes.draw do
+  #get "sessions/new"
+
  resources :users
+ resources :sessions, :only => [:new, :create, :destroy]
+
 
    match '/signup',  :to => 'users#new'
-
+    match '/signout', :to => 'sessions#destroy'
    match '/about',   :to => 'pages#about'
-
+   match '/signin',  :to => 'sessions#new'
   root :to => 'pages#home'
   # The priority is based upon order of creation:
   # first created -> highest priority.
